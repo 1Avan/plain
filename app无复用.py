ee4bbbd75eee7109418f538aee0b5560ba31cf78
流川枫 <1965564484@qq.com>
2023-12-21 10:49:16 +0800

    改善了复用依赖添加了

--- app无复用.py
+++ app无复用.py
@@ -41,9 +41,10 @@ def crawlingFn(url):
     text_content = remove_punctuations(text_content)
     text_content = remove_stopwords(text_content)
     return text_content
-def textFn():
+def page_home():
+    # 这是主页面
     st.title('欢迎使用网页词频可视化工具! 👋')
-    input_url = st.text_input("Enter URL:")
+    input_url= st.text_input("Enter URL:")
     if input_url.strip() == "":
         return
     else:
@@ -61,82 +62,168 @@ def textFn():
         filtered_word_counts = {word: freq for word, freq in word_counts.items() if freq >= min_freq}
         # 字典按值从大到小取前20个
         word_counts_20 = dict(sorted(filtered_word_counts.items(), key=lambda x: x[1], reverse=True)[:20])
-        return word_counts_20
-def page_home():
-    # 这是主页面
-    word_counts_20 = textFn()
-    bar = Bar()
-    val = list(map(int, word_counts_20.values()))
-    wordList = list(word_counts_20.keys())
-    bar.add_xaxis(wordList)
-    bar.add_yaxis("关键词", val)
-    # 设置 x 轴标签旋转角度为 45 度
-    bar.set_global_opts(
-        xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=45))
-    )
-    # 使用 st_echarts.st_pyecharts() 方法将图表渲染到 Streamlit 中
-    st_echarts.st_pyecharts(bar)
+        bar = Bar()
+        val = list(map(int, word_counts_20.values()))
+        wordList = list(word_counts_20.keys())
+        bar.add_xaxis(wordList)
+        bar.add_yaxis("关键词", val)
+        # 设置 x 轴标签旋转角度为 45 度
+        bar.set_global_opts(
+            xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=45))
+        )
+        # 使用 st_echarts.st_pyecharts() 方法将图表渲染到 Streamlit 中
+        st_echarts.st_pyecharts(bar)
 def page_ciyun():
     # 词云数据
-    word_counts = textFn()
-    # 字典按值从大到小取前20个
-    word_counts_20 = dict(sorted(word_counts.items(), key=lambda x: x[1], reverse=True))
-    wordcloud = WordCloud()
-    wordcloud.add(
-        "",
-        list(word_counts_20.items()),
-        word_size_range=[20, 100]
-    )
-    wordcloud.set_global_opts(title_opts=opts.TitleOpts(title="WordCloud Chart"))
-    st_echarts.st_pyecharts(wordcloud)
+    st.title('欢迎使用网页词频可视化工具! 👋')
+    input_url = st.text_input("Enter URL:")
+    if input_url.strip() == "":
+        return
+    else:
+        text = crawlingFn(input_url)
+        words = jieba.lcut(text)  # 使用精确模式对文本进行分词
+        word_counts = {}
+        # 获取词频字典
+        for word in words:
+            if len(word) == 1:
+                continue
+            else:
+                word_counts[word] = word_counts.get(word, 0) + 1
+        # [()]
+        # 添加交互过滤低频词的功能
+        min_freq = st.slider("设置最低词频阈值", 0, max(word_counts.values()), 0)
+        filtered_word_counts = {word: freq for word, freq in word_counts.items() if freq >= min_freq}
+        # 字典按值从大到小取前20个
+        word_counts_20 = dict(sorted(filtered_word_counts.items(), key=lambda x: x[1], reverse=True))
+        wordcloud = WordCloud()
+        wordcloud.add(
+            "",
+            list(word_counts_20.items()),
+            word_size_range=[20, 100]
+        )
+        wordcloud.set_global_opts(title_opts=opts.TitleOpts(title="WordCloud Chart"))
+        st_echarts.st_pyecharts(wordcloud)
 def page_pie():
     # 饼状图页面
-    word_counts_20 = textFn()
-    word_list = [(x,y) for x,y in word_counts_20.items()]
-    pie = pyecharts.charts.Pie()
-    pie.add("",word_list, radius=["40%", "75%"])
-    pie.set_global_opts(title_opts=opts.TitleOpts(title=""))
-    st_echarts.st_pyecharts(pie)
+    st.title('欢迎使用网页词频可视化工具! 👋')
+    input_url = st.text_input("Enter URL:")
+    if input_url.strip() == "":
+        return
+    else:
+        text = crawlingFn(input_url)
+        words = jieba.lcut(text)  # 使用精确模式对文本进行分词
+        word_counts = {}
+        # 获取词频字典
+        for word in words:
+            if len(word) == 1:
+                continue
+            else:
+                word_counts[word] = word_counts.get(word, 0) + 1
+        # 字典按值从大到小取前20个
+        # 添加交互过滤低频词的功能
+        min_freq = st.slider("设置最低词频阈值", 0, max(word_counts.values()), 0)
+        word_counts = {word: freq for word, freq in word_counts.items() if freq >= min_freq}
+        word_counts_20 = dict(sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:20])
+        # [()]
+        word_list = [(x,y) for x,y in word_counts_20.items()]
+        pie = pyecharts.charts.Pie()
+        pie.add("",word_list, radius=["40%", "75%"])
+        pie.set_global_opts(title_opts=opts.TitleOpts(title=""))
+        st_echarts.st_pyecharts(pie)
 def page_broken():
     # 这是折线图
-    word_counts_20 = textFn()
-    val = list(map(int, word_counts_20.values()))
-    wordList = list(word_counts_20.keys())
-    line = pyecharts.charts.Line()
-    line.add_xaxis(wordList)
-    line.add_yaxis("关键词",val)
-    # 设置 x 轴标签旋转角度为 45 度
-    line.set_global_opts(
-        xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=45))
-    )
-    st_echarts.st_pyecharts(line)
+    st.title('欢迎使用网页词频可视化工具! 👋')
+    input_url= st.text_input("Enter URL:")
+    if input_url.strip() == "":
+        return
+    else:
+        text = crawlingFn(input_url)
+        words = jieba.lcut(text)  # 使用精确模式对文本进行分词
+        word_counts = {}
+        # 获取词频字典
+        for word in words:
+            if len(word) == 1:
+                continue
+            else:
+                word_counts[word] = word_counts.get(word, 0) + 1
+        # 添加交互过滤低频词的功能
+        min_freq = st.slider("设置最低词频阈值", 0, max(word_counts.values()), 0)
+        word_counts = {word: freq for word, freq in word_counts.items() if freq >= min_freq}
+        # 字典按值从大到小取前20个
+        word_counts_20 = dict(sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:20])
+        val = list(map(int, word_counts_20.values()))
+        wordList = list(word_counts_20.keys())
+        line = pyecharts.charts.Line()
+        line.add_xaxis(wordList)
+        line.add_yaxis("关键词",val)
+        # 设置 x 轴标签旋转角度为 45 度
+        line.set_global_opts(
+            xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=45))
+        )
+        st_echarts.st_pyecharts(line)
 def page_point():
     # 这是散点图
-    word_counts_20 = textFn()
-    val = list(map(int, word_counts_20.values()))
-    wordList = list(word_counts_20.keys())
-    size_data = [10, 20, 30, 40, 50, 60]
-    es = pyecharts.charts.EffectScatter()
-    es.add_xaxis(wordList)
-    es.add_yaxis("关键词",val,symbol_size=size_data)
-    # 设置 x 轴标签旋转角度为 45 度
-    es.set_global_opts(
-        xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=45))
-    )
-    st_echarts.st_pyecharts(es)
+    st.title('欢迎使用网页词频可视化工具! 👋')
+    input_url= st.text_input("Enter URL:")
+    if input_url.strip() == "":
+        return
+    else:
+        text = crawlingFn(input_url)
+        words = jieba.lcut(text)  # 使用精确模式对文本进行分词
+        word_counts = {}
+        # 获取词频字典
+        for word in words:
+            if len(word) == 1:
+                continue
+            else:
+                word_counts[word] = word_counts.get(word, 0) + 1
+        # 添加交互过滤低频词的功能
+        min_freq = st.slider("设置最低词频阈值", 0, max(word_counts.values()), 0)
+        word_counts = {word: freq for word, freq in word_counts.items() if freq >= min_freq}
+        # 字典按值从大到小取前20个
+        word_counts_20 = dict(sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:20])
+        val = list(map(int, word_counts_20.values()))
+        wordList = list(word_counts_20.keys())
+        size_data = [10, 20, 30, 40, 50, 60]
+        es = pyecharts.charts.EffectScatter()
+        es.add_xaxis(wordList)
+        es.add_yaxis("关键词",val,symbol_size=size_data)
+        # 设置 x 轴标签旋转角度为 45 度
+        es.set_global_opts(
+            xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=45))
+        )
+        st_echarts.st_pyecharts(es)
 def page_box():
     # 这是箱型图
-    word_counts_20 = textFn()
-    val = list(map(int, word_counts_20.values()))
-    wordList = list(word_counts_20.keys())
-    boxplot = pyecharts.charts.Boxplot()
-    boxplot.add_xaxis(wordList)
-    boxplot.add_yaxis("关键词", val)
-    # 设置 x 轴标签旋转角度为 45 度
-    boxplot.set_global_opts(
-        xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=45))
-    )
-    st_echarts.st_pyecharts(boxplot)
+    st.title('欢迎使用网页词频可视化工具! 👋')
+    input_url = st.text_input("Enter URL:")
+    if input_url.strip() == "":
+        return
+    else:
+        text = crawlingFn(input_url)
+        words = jieba.lcut(text)  # 使用精确模式对文本进行分词
+        word_counts = {}
+        # 获取词频字典
+        for word in words:
+            if len(word) == 1:
+                continue
+            else:
+                word_counts[word] = word_counts.get(word, 0) + 1
+        # 添加交互过滤低频词的功能
+        min_freq = st.slider("设置最低词频阈值", 0, max(word_counts.values()), 0)
+        word_counts = {word: freq for word, freq in word_counts.items() if freq >= min_freq}
+        # 字典按值从大到小取前20个
+        word_counts_20 = dict(sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:20])
+        val = list(map(int, word_counts_20.values()))
+        wordList = list(word_counts_20.keys())
+        boxplot = pyecharts.charts.Boxplot()
+        boxplot.add_xaxis(wordList)
+        boxplot.add_yaxis("关键词", val)
+        # 设置 x 轴标签旋转角度为 45 度
+        boxplot.set_global_opts(
+            xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=45))
+        )
+        st_echarts.st_pyecharts(boxplot)
 def page_movie():
     df_index = gp.douban_movie_list()
     df_index2 = gp.douban_week_praise_list()
